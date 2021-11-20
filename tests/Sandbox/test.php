758dad8bdf48bb4f6357747de0f4fe85ddf559ca
Marco Hernandez <marcohern@gmail.com>
2021-11-20 15:51:51 -0500

    added html samples

--- tests/Sandbox/test.php
+++ tests/Sandbox/test.php
@@ -3,7 +3,6 @@ use Marcohern\Menko\Retail\MercadoLibre\MercadoLibreProductListScrapper;
 
 require_once('vendor/autoload.php');
 
-
 $gs = new MercadoLibreProductListScrapper();
 $r = $gs->scrap("https://listado.mercadolibre.com.co/rtx-3080-ti");
 //$gs->saveBodyTo('samples/ml.html');
